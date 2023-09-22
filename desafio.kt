b5738d1f081a76f469ab67b00ef1b27257d7bf64
Jonathas Cruz <111304620+jrcruz1998@users.noreply.github.com>
2023-09-22 18:23:40 -0300

    Desafio de projeto Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,58 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val curso: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    infix fun matricular(usuario: Usuario) { 
+        inscritos.add(usuario) 
+    }
+    
+    fun matricular(vararg usuarios: Usuario) {
+        for (usuario in usuarios) {
+            inscritos.add(usuario)
+        }
+    }
+    fun mostrarConteudo() {
+    	for (conteudo in conteudos) {
+    		println(conteudo.nome)
+    	}
+    }
+        
+    fun mostrarInscritos(){
+    	for(inscrito in inscritos) {
+    		println(inscrito.nome)
+    	}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val listaConteudo: List<ConteudoEducacional> = listOf(
+            ConteudoEducacional("Lógica de Programação", "Java"),
+            ConteudoEducacional("Linguagem de Programação", "Java"),
+            ConteudoEducacional("Orientação a Objetos", "Kotlin"),
+            ConteudoEducacional("Arquiteturas de Software", "Fundamentos Software"))
+    
+    val jonathas = Usuario("Jonathas")
+    val guilherme = Usuario("Gulherme")
+    val edson = Usuario("Edson")
+    
+    val experiencia = Formacao("Programador Kotlin", listaConteudo, Nivel.INTERMEDIARIO)
+    
+    listaConteudo?.let {
+        experiencia.matricular(jonathas, guilherme, edson)
+    }
+    
+    println("Formação: \n${experiencia.nome}")
+    println("")
+    println("Conteúdos: ")
+    experiencia.mostrarConteudo()
+    println("")
+    println("Inscritos:")
+    experiencia.mostrarInscritos()
 }
